1c2d81db5e83189db912823aa23a3e3765552e8b
wuhh <wuhh@ymify.com>
2022-07-23 18:01:55 +0800

    commit test

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+/// <reference types="vitest"></reference>
+import { configDefaults } from 'vitest/config'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import prismjs from 'vite-plugin-prismjs'
@@ -21,5 +23,9 @@ export default defineConfig({
             '@components': '/src/components',
             '@views': '/src/views'
         }
+    },
+    test: {
+        exclude: [...configDefaults.exclude, 'assets/regex.js'],
+        coverage: ['html']
     }
 })
