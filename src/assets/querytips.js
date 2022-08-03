712f352abf7760586d47fe18c7ab9f90d68e8414
wuhh <wuhh@ymify.com>
2022-08-03 17:39:39 +0800

    Modify: Framework View search function

---
+++ src/assets/querytips.js
@@ -0,0 +1,48 @@
+const FrameworkSuggestions = {
+    searchType: [
+        // tags
+        { value: '@name:' },
+        { value: '@tag:' },
+
+        // detail name
+        { value: '@name:Boostrap 3' },
+        { value: '@name:Boostrap 4' },
+        { value: '@name:Element UI' },
+        { value: '@name:Element Plus' },
+        { value: '@name:ECMAScript 2016' },
+        { value: '@name:TypeScript' },
+        { value: '@name:jQuery' },
+        { value: '@name:Vue2' },
+        { value: '@name:Vue3' },
+        { value: '@name:Angular' },
+        { value: '@name:React' },
+        { value: '@name:Backbone' },
+        { value: '@name:Laravel' },
+        { value: '@name:CakePHP' },
+        { value: '@name:Zend' },
+        { value: '@name:Symfony' },
+        { value: '@name:Yii2' },
+        { value: '@name:ThinkPHP6.0' },
+        { value: '@name:Phalcon' },
+        { value: '@name:PHP' },
+        { value: '@name:Python' },
+        { value: '@name:Go' },
+        { value: '@name:Flutter' },
+        { value: '@name:Weex' },
+        { value: '@name:Electron' },
+        { value: '@name:Qt5' },
+        { value: '@name:PyQt5' },
+        // tag
+        { value: '@tag:ui-framework' },
+        { value: '@tag:js' },
+        { value: '@tag:js-framework' },
+        { value: '@tag:php-framework' },
+        { value: '@tag:language' },
+        { value: '@tag:cross-platform' }
+    ]
+}
+
+
+export {
+    FrameworkSuggestions
+}
